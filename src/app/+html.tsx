3dc949e579a46d397fdb299709a58165490f6fb2
Khalid Ali <khalludi123@gmail.com>
2024-03-26 12:46:49 -0400

    Fix some code analysis

--- src/app/+html.tsx
+++ src/app/+html.tsx
@@ -8,6 +8,7 @@ export default function Root({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
+        <title>Food Ordering App</title>
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
