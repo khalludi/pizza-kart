111f2083fd696b73da65b3adb0ce01ca06546639
Khalid Ali <khalludi123@gmail.com>
2024-03-29 01:29:27 -0400

    Add total to shopping cart

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,10 +1,11 @@
-import { FlatList, Platform, View } from "react-native";
+import { FlatList, Platform, View, Text } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { useCart } from "@/providers/CartProvider";
 import CartListItem from "@components/CartListItem";
+import Button from "@components/Button";
 
 const CartScreen = () => {
-  const { items } = useCart();
+  const { items, total } = useCart();
 
   return (
     <View>
@@ -13,6 +14,11 @@ const CartScreen = () => {
         renderItem={({ item }) => <CartListItem cartItem={item} />}
         contentContainerStyle={{ padding: 10, gap: 10 }}
       />
+
+      <Text style={{ marginTop: 20, fontSize: 20, fontWeight: "500" }}>
+        Total: ${total.toFixed(2)}
+      </Text>
+      <Button text={"Checkout"} />
 
       {/* Use a light status bar on iOS to account for the black space above the modal */}
       <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
