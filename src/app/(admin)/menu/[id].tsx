a640cb1632883bfb3aa10e5cfbe61c1aa1aad9b3
Khalid Ali <khalludi123@gmail.com>
2024-04-03 01:21:15 -0400

    Use `react-query` to select products from Supabase

--- src/app/(admin)/menu/[id].tsx
+++ src/app/(admin)/menu/[id].tsx
@@ -1,4 +1,11 @@
-import { View, Text, Image, StyleSheet, Pressable } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  Pressable,
+  ActivityIndicator,
+} from "react-native";
 import { Link, Stack, useLocalSearchParams, useRouter } from "expo-router";
 import products from "@assets/data/products";
 import React, { useState } from "react";
@@ -6,17 +13,19 @@ import { useCart } from "@/providers/CartProvider";
 import { PizzaSize } from "@/types";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import Colors from "@/constants/Colors";
+import { useProduct } from "@/api/products";
 
 const sizes: PizzaSize[] = ["S", "M", "L", "XL"];
 
 const ProductDetailsScreen = () => {
-  const { id } = useLocalSearchParams();
+  const { id: idString } = useLocalSearchParams();
+  const id = parseFloat(typeof idString === "string" ? idString : idString[0]);
+  const { data: product, error, isLoading } = useProduct(id);
+
   const { addItem } = useCart();
   const router = useRouter();
 
   const [selectedSize, setSelectedSize] = useState<PizzaSize>("M");
-
-  const product = products.find((p) => p.id.toString() === id);
 
   const addToCart = () => {
     if (!product) return;
@@ -26,6 +35,14 @@ const ProductDetailsScreen = () => {
 
   if (!product) {
     return <Text>Product not found</Text>;
+  }
+
+  if (isLoading) {
+    return <ActivityIndicator />;
+  }
+
+  if (error) {
+    return <Text>Failed to fetch products</Text>;
   }
 
   return (
